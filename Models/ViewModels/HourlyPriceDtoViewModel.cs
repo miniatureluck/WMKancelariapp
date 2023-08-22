30bc2fa84df33dc9254fd6b08263f8cd37db6e11
miniatureluck <105822819+miniatureluck@users.noreply.github.com>
2023-08-22 11:08:54 +0200

    hourlyprice index view grouping by case

--- Models/ViewModels/HourlyPriceDtoViewModel.cs
+++ Models/ViewModels/HourlyPriceDtoViewModel.cs
@@ -1,12 +1,22 @@
-﻿namespace WMKancelariapp.Models.ViewModels
+﻿using System.ComponentModel;
+
+namespace WMKancelariapp.Models.ViewModels
 {
     public class HourlyPriceDtoViewModel
     {
         public string HourlyPriceId { get; set; }
+        
+        [DisplayName("Stawka")]
         public int Price { get; set; }
         public User User { get; set; }
+
+        [DisplayName("Czynność")]
         public TaskType TaskType { get; set; }
+
+        [DisplayName("Sprawa")]
         public Case Case { get; set; }
+
+        [DisplayName("Data modyfikacji")]
         public DateTime LastModified { get; set; } = DateTime.Now;
 
         public List<UserTask> UserTasks { get; set; } = new List<UserTask>();
